f3e82278284f4e63fcc9519b1c3e704f7239f54f
Thor Anker Kvisgård Lange <tal@netic.dk>
2021-09-08 12:25:28 +0200

    chore: Rewriting to be able to run demo on aws-eks cluster

--- demo.sh
+++ demo.sh
@@ -5,46 +5,46 @@ script_dir=$(dirname $0)
 pushd $script_dir
 
 function commit_and_push() {
-  git add gotk/clusters/azure-aks/bootstrap/sync.yaml
+  git add gotk/clusters/$2/bootstrap/sync.yaml
   git commit -m "chore: $1"
   git push
 }
 
 function cleanup() {
   echo Resetting cluster
-  cp manifests/cluster/sync_basic.yaml gotk/clusters/azure-aks/bootstrap/sync.yaml
-  commit_and_push "Reset demo cluster configuration"
+  cp manifests/cluster/$1/sync_basic.yaml gotk/clusters/$1/bootstrap/sync.yaml
+  commit_and_push $1 "Reset demo cluster configuration"
 }
 
 function observable() {
   echo Making cluster observable
-  cp manifests/cluster/sync_observable.yaml gotk/clusters/azure-aks/bootstrap/sync.yaml
-  commit_and_push "Configure demo cluster for observability"
+  cp manifests/cluster/$1/sync_observable.yaml gotk/clusters/$1/bootstrap/sync.yaml
+  commit_and_push $1 "Configure demo cluster for observability"
 }
 
 function namespace() {
   echo Making application namespace for devops team and removing app deployment
-  kubectl --kubeconfig terraform/azure-aks/kube_config.yaml delete -k manifests/app/observable
-  cp manifests/cluster/sync_full.yaml gotk/clusters/azure-aks/bootstrap/sync.yaml
-  commit_and_push "Configure demo cluster with application namespace"
+  kubectl --kubeconfig terraform/$1/kube_config.yaml delete -k manifests/app/observable
+  cp manifests/cluster/$1/sync_full.yaml gotk/clusters/$1/bootstrap/sync.yaml
+  commit_and_push $1 "Configure demo cluster with application namespace"
 }
 
 function deploy_basic() {
   echo Deploying hotrod without observability
-  kubectl --kubeconfig terraform/azure-aks/kube_config.yaml apply -k manifests/app/basic
+  kubectl --kubeconfig terraform/$1/kube_config.yaml apply -k manifests/app/basic
 }
 
 function deploy_observable() {
   echo Deploying hotrod with observability
-  kubectl --kubeconfig terraform/azure-aks/kube_config.yaml apply -k manifests/app/otel
-  kubectl --kubeconfig terraform/azure-aks/kube_config.yaml label ns default netic.dk/monitoring="true"
+  kubectl --kubeconfig terraform/$1/kube_config.yaml apply -k manifests/app/otel
+  kubectl --kubeconfig terraform/$1/kube_config.yaml label ns default netic.dk/monitoring="true"
   sleep 5
-  kubectl --kubeconfig terraform/azure-aks/kube_config.yaml apply -k manifests/app/observable
+  kubectl --kubeconfig terraform/$1/kube_config.yaml apply -k manifests/app/observable
 }
 
 function remove_app() {
   echo Removing hotrod deployment
-  kubectl --kubeconfig terraform/azure-aks/kube_config.yaml delete -k manifests/app/observable
+  kubectl --kubeconfig terraform/$1/kube_config.yaml delete -k manifests/app/observable
 }
 
 while [[ $# -gt 0 ]]; do
@@ -53,27 +53,27 @@ while [[ $# -gt 0 ]]; do
   case $key in
     cleanup)
     shift
-    cleanup
+    cleanup $1
     ;;
     observable)
     shift
-    observable
+    observable $1
     ;;
     namespace)
     shift
-    namespace
+    namespace $1
     ;;
     app_basic)
     shift
-    deploy_basic
+    deploy_basic $1
     ;;
     app_observable)
     shift
-    deploy_observable
+    deploy_observable $1
     ;;
     app_remove)
     shift
-    remove_app
+    remove_app $1
     ;;
     *)
     echo Unknown argument: $key
